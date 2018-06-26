fd1c7fc65db26f85595e8d4f4692d4be2d6184e8
Devin Ryan <devin.ryan514@gmail.com>
2018-06-26 01:56:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ Handlebars.registerHelper('comment_body', function() {
     return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
   }
 })
+
+function loadIssues() {
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
